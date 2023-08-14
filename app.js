5f45bc67bef0e816de13fff42af0ddd89c643fed
Damien <yithanglee@gmail.com>
2023-08-14 11:24:26 +0800

    added helment

--- app.js
+++ app.js
@@ -3,12 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require("helmet");
+
+
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'openstreetmap.org'],
+      styleSrc: ["'self'", 'openstreetmap.org'],
+      imgSrc: ["'self'", 'tile.openstreetmap.org', '*.tile.openstreetmap.org'],
+      connectSrc: ["'self'", 'openstreetmap.org'],
+      fontSrc: ["'self'", 'openstreetmap.org'],
+    },
+  }),
+);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 5 * 60 * 1000, // 1 minute
+  max: 100,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,12 +54,12 @@ app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
