9067e52a2c6827d687215d07b52ae229c65c7df5
Damien <yithanglee@gmail.com>
2024-09-23 18:19:43 +0800

    sle

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       // Allow form submissions to the domain (without specifying the exact path)
-      "form-action": ["'self'", "https://blog.damienslab.com", "https://payment.ipay88.com.my", "http://localhost:4000"],
+      "form-action": ["'self'", "https://https://iot.djtech4u.com", "https://blog.damienslab.com", "https://payment.ipay88.com.my", "http://localhost:4000"],
       
       // Other CSP directives
       defaultSrc: ["'self'"],
