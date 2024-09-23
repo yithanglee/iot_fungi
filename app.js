7903338783fe94d36ffda35bf0316d2db236a3d6
Damien <yithanglee@gmail.com>
2024-09-23 18:33:41 +0800

    gi

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       // Allow form submissions to the domain (without specifying the exact path)
-      "form-action": ["*"],
+      "form-action": ["'self'", "https://blog.damienslab.com", "https://payment.ipay88.com.my", "http://localhost:4000"],
+      
       // Other CSP directives
       defaultSrc: ["'self'"],
       "script-src-attr": ["'self'", "'unsafe-inline'"],
